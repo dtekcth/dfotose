2a1ccf96090f1f4fb20af205fe57df47d2eb2ba6
sebastian <me@sebastianlund.se>
2016-09-09 13:09:53 +0200

    added kerberos setup script

---
+++ setup-kerberos.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+krb5_conf="""[libdefaults]
+  default_realm = CHALMERS.SE
+  clockskew = 300
+  v4_instance_resolve = false
+  dns_lookup_kdc = true
+
+[domain_realm]
+  .chalmers.se = CHALMERS.SE
+  chalmers.se = CHALMERS.SE
+[logging]
+  default = SYSLOG:INFO:USER
+"""
+
+echo "WARNING: overwriting /etc/krb5.conf"
+echo "NOTE: old is backed up at /etc/krb5.conf.bak"
+mv /etc/krb5.conf{,.bak}
+echo -e "$krb5_conf" > /etc/krb5.conf
