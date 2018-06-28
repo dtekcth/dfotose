0a8811f6d0166fb534aa5af3f6204d6198bc379b
Christoffer Arvidsson <26621143+Eethern@users.noreply.github.com>
2018-06-28 19:00:18 +0200

    Center pages (#22)
    * centered the pages, changed flexbox to grid
    
    * Fixed gallery card overlay not showing
    
    * changed grids
    
    * general cleanup
    
    * centered about page
    
    * minor changes
    
    * reapplied hover fx
    
    * init
    
    * centered tag-search
    
    * cleanup

--- src/client/App.js
+++ src/client/App.js
@@ -61,13 +61,15 @@ const Admin = ({children}) => {
 
 const About = () => {
   return (
-    <div className="site-content about-us">
-      <h2>Om oss</h2>
-      <p>DFoto är Datateknologsektionens fotoförening.
-        Vår ambition är att genom foto och film föreviga alla arrangemang Datateknologen går på.
-        Allt ifrån iDrotts legendariska aktiviteter, till D6s storslagna fester och Deltas episka pubar.</p>
-      <p> Du kan komma i kontakt med oss genom att maila <a href="mailto:dfoto@dtek.se">dfoto@dtek.se</a>!</p>
-      <h3>Vi ses genom kameralinsen!</h3>
+    <div className="wrapper">
+      <div className="site-content about-us">
+        <h2>Om oss</h2>
+        <p>DFoto är Datateknologsektionens fotoförening.
+          Vår ambition är att genom foto och film föreviga alla arrangemang Datateknologen går på.
+          Allt ifrån iDrotts legendariska aktiviteter, till D6s storslagna fester och Deltas episka pubar.</p>
+        <p> Du kan komma i kontakt med oss genom att maila <a href="mailto:dfoto@dtek.se">dfoto@dtek.se</a>!</p>
+        <h3>Vi ses genom kameralinsen!</h3>
+      </div>
     </div>
   );
 };
