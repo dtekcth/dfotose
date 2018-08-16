91335a4f8c813f90710ae7ce11f754c9edf1a512
sebastian <me@sebastianlund.se>
2018-08-16 15:36:29 +0200

    fix webpack after update

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   devtool: 'eval',
+  mode: 'development',
   entry: [
     'react-hot-loader/patch',
     'webpack/hot/dev-server',
@@ -18,10 +19,10 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   resolve: {
-    extensions: ['', '.js']
+    extensions: ['.js']
   },
   module: {
-    loaders:[
+    rules:[
       {
         //test: /\.js?$/,
         test: /\.jsx?$/,
@@ -48,7 +49,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
+        loaders: ["style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.json$/,
