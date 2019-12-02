5067718d581ebbfd603270913067af6e8f414fb0
mightynerd <staszw@gmail.com>
2019-12-02 17:19:53 +0100

    Add facebook link to the about page

--- src/client/App.js
+++ src/client/App.js
@@ -54,7 +54,9 @@ const About = () => {
         <p>DFoto är Datateknologsektionens fotoförening.
           Vår ambition är att genom foto och film föreviga alla arrangemang Datateknologen går på.
           Allt ifrån iDrotts legendariska aktiviteter, till D6s storslagna fester och Deltas episka pubar.</p>
-        <p> Du kan komma i kontakt med oss genom att maila <a href="mailto:dfoto@dtek.se">dfoto@dtek.se</a>!</p>
+        <p> Du kan komma i kontakt med oss genom att maila <a href="mailto:dfoto@dtek.se">dfoto@dtek.se</a> eller 
+          skriva till vår <a href="https://www.facebook.com/dfotochalmers/">Facebooksida</a>!
+        </p>
         <h3>Vi ses genom kameralinsen!</h3>
       </div>
     </div>
